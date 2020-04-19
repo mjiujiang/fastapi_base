a8225b93faf3de0ffd421717fe634e689aa21dda
mjiujiang <312061273@qq.com>
2020-04-19 21:05:51 +0800

    first commit

---
+++ app/database/__init__.py
@@ -0,0 +1,9 @@
+from app.database.database import SessionLocal
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
---
+++ main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app import api_user
+
+from app.database import models
+from app.database.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def create_app():
+    fast_app = FastAPI(debug=True)
+    fast_app.include_router(api_user.router)
+
+    return fast_app
+
+
+app = create_app()
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', port=8000, debug=True, reload=True, lifespan='on')
